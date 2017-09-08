b430004ff143d089a4e1c3a2e91bb53817e37a30
Ana Guerrero López <ana-externe.guerrero@edf.fr>
2017-09-08 15:07:26 +0000

    Rename to libpam-mkhomedir-scibian
    Also made a few packaging updates

--- pam_mkhomedir.py
+++ pam_mkhomedir.py
@@ -3,14 +3,14 @@
 #
 # Copyright (C) 2017 EDF SA
 #
-# This file is part of libpam-mkhomedir-hpc.
+# This file is part of libpam-mkhomedir-scibian.
 #
-# libpam-mkhomedir-hpc is free software: you can redistribute it and/or
+# libpam-mkhomedir-scibian is free software: you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
 #
-# libpam-mkhomedir-hpc is distributed in the hope that it will be
+# libpam-mkhomedir-scibian is distributed in the hope that it will be
 # useful, but WITHOUT ANY WARRANTY; without even the implied warranty
 # of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
